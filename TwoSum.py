dc9b4bca4ec6fc16d768f1dc35654a86cd591970
Naga Sai Srinivas <srinivas.ycplf8@gmail.com>
2020-07-07 00:13:57 +0530

    Adding Two linked lists to get Sum

--- TwoSum.py
+++ TwoSum.py
@@ -1,12 +1,36 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        counts={}
+    def __init__(self):
+        self.head=None
         
-        nums.sort()
-        for i in nums:
-            difference=target-i
-            if difference not in counts:
-                counts[i]=1
-            else:
-                starting=nums.index(difference)
-                return nums.index(difference),nums.index(i,starting+1)
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        current=l1
+        new=[]
+        while current!=None:
+            new.append(current.val)
+            current=current.next
+        current2=l2
+        new2=[]
+        while current2!=None:
+            new2.append(current2.val)
+            current2=current2.next
+
+        res1 = ("".join(map(str, new[::-1]))) 
+        res2 = ("".join(map(str, new2[::-1]))) 
+        result=str(int(res1)+int(res2))
+        final=result[::-1]
+        final=(list(map(int, final))) 
+        i=0
+        result=ListNode(0)
+        resulttail=result
+        while i<len(final):
+            resulttail.next=ListNode(final[i])
+            i+=1
+            resulttail=resulttail.next
+        
+        return result.next
+            
